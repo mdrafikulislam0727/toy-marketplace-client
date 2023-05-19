e752b2002d009a4ffbe19b0e36ba74049cf28d8c
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-05-19 07:21:18 +0600

    added the add toy form

--- src/component/AddAToy/AddAToy.jsx
+++ src/component/AddAToy/AddAToy.jsx
@@ -1,8 +1,89 @@
 
 const AddAToy = () => {
     return (
-        <div>
-            <h1>This is a add toy</h1>
+       <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-3xl font-extrabold text-center my-6">Add A Toy</h2>
+            <form>
+                {/* form name and quantity row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Toy Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Toy name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Seller Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="seller" placeholder="Seller Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form supplier row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Sub_category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="category" placeholder="category" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="price" placeholder="Price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form category and details row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="quantity" placeholder="quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="details" placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form category and details row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Photo Url</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="url" name="photo" placeholder="Photo Url" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Rating</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="rating" placeholder="Rating" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form photo url  row */}
+              
+                <input type="submit" value="Add " className="btn btn-block" />
+            </form>
         </div>
     );
 };
