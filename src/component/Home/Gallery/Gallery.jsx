f8cef09cb264f70ef9c5c35c703dbda08cbe053d
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-05-19 12:14:11 +0600

    modefay glary sesction

--- src/component/Home/Gallery/Gallery.jsx
+++ src/component/Home/Gallery/Gallery.jsx
@@ -1,14 +1,17 @@
-import img1 from '../../../assets/banner1.png'
-import img2 from '../../../assets/banner2.png'
-import img3 from '../../../assets/banner3.png'
-import img4 from '../../../assets/banner4.png'
+import gallery1 from '../../../assets/gllary1.png'
+import gallery2 from '../../../assets/gllary2.png'
+import gallery3 from '../../../assets/gllary3.png'
+import gallery4 from '../../../assets/gllary4.png'
+import gallery5 from '../../../assets/gllary5.png'
+import gallery6 from '../../../assets/gllary6.png'
+import gallery7 from '../../../assets/gllary7.png'
 const Gallery = () => {
     return (
         <div className="carousel w-full h-[300px] my-24">
             <div id="slide1" className="carousel-item relative w-full">
                 <div className='flex w-full'>
-                    <img src={img2} alt="" className='w-1/2 rounded-xl' />
-                    <img src={img1} className="w-1/2 rounded-xl" />
+                    <img src={gallery1} alt="" className='w-1/2 rounded-xl' />
+                    <img src={gallery2} className="w-1/2 rounded-xl" />
                 </div>
                 <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12  w-1/2'>
@@ -23,8 +26,8 @@ const Gallery = () => {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <div className='flex w-full'>
-                    <img src={img3} alt="" className='w-1/2 rounded-xl' />
-                    <img src={img1} className="w-1/2 rounded-xl" />
+                    <img src={gallery3} alt="" className='w-1/2 rounded-xl' />
+                    <img src={gallery4} className="w-1/2 rounded-xl" />
                 </div>            <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12  w-1/2'>
 
@@ -37,8 +40,8 @@ const Gallery = () => {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <div className='flex w-full'>
-                    <img src={img4} alt="" className='w-1/2 rounded-xl' />
-                    <img src={img1} className="w-1/2 rounded-xl" />
+                    <img src={gallery5} alt="" className='w-1/2 rounded-xl' />
+                    <img src={gallery6} className="w-1/2 rounded-xl" />
                 </div>                <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12  w-1/2'>
 
@@ -51,8 +54,8 @@ const Gallery = () => {
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <div className='flex w-full'>
-                    <img src={img2} alt="" className='w-1/2 rounded-xl' />
-                    <img src={img1} className="w-1/2 rounded-xl" />
+                    <img src={gallery7} alt="" className='w-1/2 rounded-xl' />
+                    <img src={gallery3} className="w-1/2 rounded-xl" />
                 </div>                <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12  w-1/2'>
 
